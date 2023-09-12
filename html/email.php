7052917c852264e7297424abfe309e60a142fe67
bob190404 <97911644+bob190404@users.noreply.github.com>
2023-09-12 10:40:55 +0200

    responsiveness done
    all done

--- html/email.php
+++ html/email.php
@@ -7,21 +7,19 @@
 
     $onderwerp= $_POST['onderwerp'];
 
-    $to = $email;
+    $to = "bob190404@gmail.com";
 
     $bericht = $_POST['bericht'];
 
     $txt = $naam . "\r\n" . $email . "\r\n" . $onderwerp . "\r\n" . $bericht;
 
-    $headers = "From: bob190404@gmail.com" ;
-
-    $txt2 = $naam . "\r\n" . "Hoi! leuk dat je in contact wilt komen!! /r/n Ik heb uw vraag ontvangen en zal er binnenkort op terug komen!" . "\r\n" . "\r\n" . $email . "\r\n" . $onderwerp . "\r\n" . $bericht;
+    $txt2 = "beste " . $naam . "\r\n" . "Hoi! leuk dat je in contact wilt komen!! Ik heb uw vraag ontvangen en zal er binnenkort op terug komen!" . "\r\n" . "\r\n" . $email . "\r\n" . $onderwerp . "\r\n" . $bericht;
 
     if($email!=NULL){
 
-        mail($to,$onderwerp,$txt,$headers);
+        mail($to,$onderwerp,$txt);
 
-        mail($email,$onderwerp,$txt2,$headers);
+        mail($email,$onderwerp,$txt2);
 
     }
 
